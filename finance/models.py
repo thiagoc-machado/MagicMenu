560cb91c03a2c2d4a170761f9d643730a955decd
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-10-02 20:07:58 +0200

    model

--- finance/models.py
+++ finance/models.py
@@ -7,6 +7,7 @@ from orders.models import Order
 class Finance(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=100)
 
     def __str__(self):
         return self.description
