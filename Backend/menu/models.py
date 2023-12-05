27f719315ef4b857adb5d82e23409c246f771f81
thiagocmachado@yahoo.com.br <thiagocmachado@yahoo.com.br>
2023-12-05 15:37:06 +0100

    menu

--- Backend/menu/models.py
+++ Backend/menu/models.py
@@ -5,7 +5,7 @@ class Menu(models.Model):
     type = models.CharField(max_length=200, null=True, blank=True)
     price = models.IntegerField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
     deleted_at = models.DateTimeField(auto_now=True, null=True, blank=True)
     created_by = models.CharField(max_length=30, null=True, blank=True)
